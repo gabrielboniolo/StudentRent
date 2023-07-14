929fc6d1a13699ecf76cbfc0ce66062669bb1f83
Gabriel Boniolo <gabrielboniolo@gmail.com>
2023-07-14 10:56:49 -0300

    Aluguel de quartos para estudantes

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace StudentRent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int qty = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 1; i <= qty; i++)
+            {
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                students[room] = new Student(name, email);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for(int i = 0; i < students.Length; i++)
+            {
+                if (students[i] != null)
+                {
+                    Console.WriteLine($"{i}: {students[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StudentRent
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Student(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string? ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+    }
+}
